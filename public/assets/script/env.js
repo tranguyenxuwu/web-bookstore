2633b67ebd1b09682c03de5ea6cf463722fa26fa
tranguyenxuwu <tranguyenx@gmail.com>
2025-03-27 10:30:18 +0700

    fix endpoints

--- public/assets/script/env.js
+++ public/assets/script/env.js
@@ -2,27 +2,27 @@
 //this file is used to set the environment variables for the application
 export const APP_ENV = {
   // Base URL
-  BASE_URL: "http://localhost:3000",
+  BASE_URL: "https://api.elysia-app.live",
 
   // GET ENDPOINTS
-  FETCH_STORE_URL: "http://localhost:3000/book/randomStoreIndex",
-  SEARCH_URL: "http://localhost:3000/book/search",
-  MASTER_URL: "http://localhost:3000/book/all",
-  FETCH_BY_ID_URL: "http://localhost:3000/book/id/",
+  FETCH_STORE_URL: "https://api.elysia-app.live/book/randomStoreIndex",
+  SEARCH_URL: "https://api.elysia-app.live/book/search",
+  MASTER_URL: "https://api.elysia-app.live/book/all",
+  FETCH_BY_ID_URL: "https://api.elysia-app.live/book/id/",
 
   // POST ENDPOINTS
-  IMAGE_PRESIGNED_URL: "http://localhost:3000/upload/presigned",
-  UPLOAD_BOOK_URL: "http://localhost:3000/upload/book",
+  IMAGE_PRESIGNED_URL: "https://api.elysia-app.live/upload/presigned",
+  UPLOAD_BOOK_URL: "https://api.elysia-app.live/upload/book",
 
   // Auth endpoints
-  LOGIN_URL: "http://localhost:3000/auth/login",
-  REGISTER_URL: "http://localhost:3000/auth/register",
+  LOGIN_URL: "https://api.elysia-app.live/auth/login",
+  REGISTER_URL: "https://api.elysia-app.live/auth/register",
 
   // placeholder image
   PLACEHOLDER_IMAGE: "https://placehold.co/400x600?text=Book+Cover",
-  LOGO_IMAGE: "http://localhost:3000/",
-  CART_ICON: "http://localhost:3000/.svg",
-  ACCOUNT_ICON: "http://localhost:3000/con.svg",
+  LOGO_IMAGE: "https://api.elysia-app.live/",
+  CART_ICON: "https://api.elysia-app.live/.svg",
+  ACCOUNT_ICON: "https://api.elysia-app.live/con.svg",
 
   DEBUG_MODE: true,
 };
